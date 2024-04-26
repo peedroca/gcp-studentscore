b30d3572462319105d66fb7c7da3dc2b96fa30bf
peedroca <phm1080@gmail.com>
2024-04-25 21:33:45 -0300

    Added bot configuration

--- discord/index.js
+++ discord/index.js
@@ -1,19 +1,47 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, ChannelType } = require('discord.js');
+const axios = require('axios');
 const client = new Client({ intents: [
     GatewayIntentBits.Guilds, 
     GatewayIntentBits.GuildMessages, 
-    GatewayIntentBits.MessageContent] 
-});
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,  
+    GatewayIntentBits.DirectMessageReactions,
+    4096
+]});
+
+const endpoint = "https://savemessage-uicelghxba-uc.a.run.app/"
 
 client.once('ready', () => {
     console.log('O bot está online!');
 });
 
 client.on('messageCreate', message => {
-    if (message.content === '!hello') {
-        message.channel.send('world');
+    if (message.content === '!join') {
+        message.author.send('Hey bro! What\'s up?');
+    }
+
+    if (message.author.bot) return;
+
+    if (message.channel.type === ChannelType.DM) {
+        const params = {
+            author: message.author.username,
+            code: process.env.CODE, 
+            response: message.content
+        };
+        
+        axios.get(endpoint, { params })
+            .then(response => {
+                const data = {
+                    content: `${message.author.username} Correct Answer! 🎉`
+                }
+                axios.post(process.env.DISCORD_WEBHOOK_SUCCESS, data)
+            })
+            .catch(error => {
+                console.error(error);
+            });// 
     }
 });
+
 
 client.login(process.env.DISCORD_BOT_TOKEN)
--- samples/cloud_functions/main.py
+++ samples/cloud_functions/main.py
@@ -1,5 +1,8 @@
 import functions_framework
+import datetime
 from google.cloud import firestore
+
+brasilia_tz = datetime.timezone(datetime.timedelta(hours=-3))
 
 @functions_framework.http
 def hello(request):
@@ -13,3 +16,23 @@ def hello(request):
         users.append(f"{doc.id} => {doc.to_dict()}")
         
     return users
+
+@functions_framework.http
+def save_message(request):
+    db = firestore.Client()
+
+    code = request.args.get("code")
+    author = request.args.get("author")
+    response = request.args.get("response")
+
+    message = {
+        "author": author, 
+        "code": code, 
+        "response": response, 
+        "update_at": datetime.datetime.now(tz=brasilia_tz)
+    }
+
+    doc_ref = db.collection("discord").document('{}_{}'.format(code, author))
+    doc_ref.set(message)
+
+    return "Ok!"
